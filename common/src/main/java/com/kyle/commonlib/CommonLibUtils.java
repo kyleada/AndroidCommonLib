416c2f416166371a29d850ad51a68f648c17b9ee
kw <kw@neusoft.com>
2016-03-08 10:15:23 +0800

    大量参考 https://github.com/DreaminginCodeZH/Douya

--- common/src/main/java/com/kyle/commonlib/CommonLibUtils.java
+++ common/src/main/java/com/kyle/commonlib/CommonLibUtils.java
@@ -1,10 +1,8 @@
 package com.kyle.commonlib;
 
 import android.content.Context;
-
 import com.kyle.commonlib.base.AppContextUtil;
 import com.kyle.commonlib.glide.GlideManager;
-import com.kyle.commonlib.utils.SpUtils;
 
 /**
  * Created by Administrator on 2016/3/5.
@@ -19,7 +17,6 @@ public class CommonLibUtils {
 
         AppContextUtil.init(context);
         //必须先完成AppContextUtil的初始化，下面部分初始化会从中提取context
-        SpUtils.init();
         GlideManager.initConfig(context);
 
     }
