7ca1fe2622b08094a076edbf34768f35e48b30d2
kyleada <wangkaidut@foxmail.com>
2016-03-09 20:54:05 +0800

    补充

--- app/src/main/java/com/kyle/App.java
+++ app/src/main/java/com/kyle/App.java
@@ -2,12 +2,15 @@ package com.kyle;
 
 import android.app.Application;
 
+import com.kyle.common.BuildConfig;
 import com.kyle.zhihuDailyPRD.DailyApp;
 
 /**
  * Created by Administrator on 2016/3/5.
  */
 public class App extends Application {
+
+    public final static boolean DEBUG = BuildConfig.DEBUG;
 
     @Override
     public void onCreate() {
